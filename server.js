69e52ba1ea4059eaa9b26b750335841fc246bbdf
Marco Mata <patrickstar1281@yahoo.com>
2022-07-13 05:42:26 -0500

    set up post request so user can enter data into api

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 const {animals} = require('./data/animals');
 const express = require('express');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 //tell heroku to run port 'process.env.PORT(this runs port 80)' if it has been set, if not default to port 3001
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//parse incoming json data
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`)
@@ -62,6 +70,20 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+
+    return animal;
+
+}
+
 app.get('/api/animals', (req, res) => {
 
     let results = animals;
@@ -85,3 +107,35 @@ app.get('/api/animals/:id', (req, res) => {
     }
 
 });
+
+app.post('/api/animals', (req, res) => {
+
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //if any data in req.body is incorrect, send a400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formattaed.')
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal)
+    }
+
+});
+
+function validateAnimal(animal) {
+    if ( !animal.name || typeof animal.anme !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray (animal.personalityTraits)) {
+        return false;
+    };
+
+    return true;
+}
